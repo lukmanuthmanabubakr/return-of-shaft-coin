57cdea58e85380449bb1b45ffdeb645ad9c1d359
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2024-01-26 12:45:46 +0100

    good

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,40 @@
-import './App.css';
+import React from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import "./App.css"; // Make sure this file contains your additional styling
+
+import day1 from "./Asset/day1.jpg";
+import grandpa from "./Asset/grandpa.jpeg";
+
+const images = [
+  day1,
+  grandpa,
+  grandpa,
+  grandpa,
+  grandpa,
+];
 
 function App() {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000, // Adjust the speed as needed
+  };
+
   return (
-    <div className="App">
-      app
+    <div className="slider-container">
+      <Slider {...settings}>
+        {images.map((image, index) => (
+          <div key={index} className="slider-item">
+            <img src={image} alt={`Slide ${index + 1}`} />
+          </div>
+        ))}
+      </Slider>
     </div>
   );
 }
